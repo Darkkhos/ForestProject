2d1683b02985adcc431903081b901d571457f93c
Darkkhos <rodriguesjohn95@gmail.com>
2024-05-01 15:29:46 -0300

    implement handle jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,12 +7,22 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D rb2D;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(-.1f, 0);
 
     public float speed;
 
+    public float forceJump = 2;
+
     private void Update()
     {
+        HandleJump();
+        HandleMovements();
+    }
+
+    
+    private void HandleMovements()
+    {
+        #region Move X
         if (UnityEngine.Input.GetKey(KeyCode.A))
         {
             //rb2D.MovePosition(rb2D.position - velocity * Time.deltaTime);
@@ -23,7 +33,28 @@ public class Player : MonoBehaviour
             //rb2D.MovePosition(rb2D.position + velocity * Time.deltaTime);
             rb2D.velocity = new Vector2(speed, rb2D.velocity.y);
         }
+        #endregion
+
+        #region Move Y
+
+        if (rb2D.velocity.x > 0)
+        {
+            rb2D.velocity -= friction;
+        }
+        else if (rb2D.velocity.x < 0)
+        {
+            rb2D.velocity += friction;
+        }
+
+        #endregion
     }
 
 
+    private void HandleJump()
+    {
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
+        {           
+            rb2D.velocity = Vector2.up * forceJump;
+        }
+    }
 }
